34fd84f2ef79575a58b4c691c9792ef4b3b33bdf
jnguyen3436 <jnguyen3436@gmail.com>
2019-10-31 14:30:18 -0400

    border detection

--- hero.js
+++ hero.js
@@ -1,120 +1,120 @@
 
 
 
-const hello = () => {
-    console.log('hi')
-}
+// const hello = () => {
+//     console.log('hi')
+// }
 
 
-class Hero {
-    constructor(x, y, hp, weapon) {
-        this.hp = hp;
-        this.x = x;
-        this.y = y;
-        this.weapon = weapon;
-    }
-    loadHero = () => {
+// class Hero {
+//     constructor(x, y, hp, weapon) {
+//         this.hp = hp;
+//         this.x = x;
+//         this.y = y;
+//         this.weapon = weapon;
+//     }
+//     loadHero = () => {
 
-    }
-}
-
-
-class Vector {
-    constructor(direction, speed) {
-        this.setDirection(direction, speed)
-    }
-
-setDirection(direction, speed) {
-    this.direction = direction;
-    this.speed = speed;
-    this.x = 0;
-    this.y = 0;
-    switch (direction) {
-        case 'up':
-            this.y = -speed;
-            break;
-
-        case 'down':
-            this.y = speed;
-            break;
-
-        case 'right':
-            this.x = -speed;
-            break;
-
-        case 'left':
-            this.x = speed;
-            break;
-    }
-}
-}
-
-class Body {
-    constructor({ imageName, speed }) {
-        this.x = 0;
-        this.y = 0;
-        this.speed = speed;
-        this.velocity = new Vector('down', 0); // vector of movement
-        this.lastTime = 0; // time of last frame
-        this.animations = {}; // container for animations
-
-        const animationSheet = new CharacterSheet({ imageName: imageName });
-        "walk_down, walk_up, walk_left, walk_right".split(',').forEach(name => {
-            this.animations[name] = animationSheet.getAnimation(name);
-    });
-    this.stand('down')
-}
-
-    walk(direction) { // its making hero walk
-        this.velocity.setDirection(direction, this.speed); // set speed
-        this.view = this.animations["walk_" + direction]; // choose speed
-        this.view.run() // run speed
-    }
-
-    stand(direction) { // its making hero stop
-        this.velocity.setDirection(direction, 0);
-        this.view = this.animations["walk_" + direction];
-        this.view.stop();
-    }
-
-    update(time) {
-        if(this.lastTime == 0) {
-            this.lastTime = time;
-              return;
-        }
-
-        this.x += (time - this.lastTime) * (this.velocity.x / 1000);
-        this.y += (time - this.lastTime) * (this.velocity.x / 1000);
-        this.lastTime = time;
-        this.view.setXY(Math.trunc(this.x),Math.trunc(this.y))
-        this.view.update(time);
-
-    }
-}
+//     }
+// }
 
 
-class Player extends Body {
+// class Vector {
+//     constructor(direction, speed) {
+//         this.setDirection(direction, speed)
+//     }
 
-    constructor(control) {
-        super({imageName: "player", speed: 50});
-        this.control = control
-    }
-    update(time) {
-        if(this.control.up) {
-            this.walk('up');
-        } else if (this.control.down) {
-            this.walk('down'); 
-        } else if (this.control.right) {
-            this.walk('right'); 
-        } else if (this.control.left) {
-            this.walk('left'); 
-        } else {
-            this.stand(this.velocity.direction);
-        }
+// setDirection(direction, speed) {
+//     this.direction = direction;
+//     this.speed = speed;
+//     this.x = 0;
+//     this.y = 0;
+//     switch (direction) {
+//         case 'up':
+//             this.y = -speed;
+//             break;
 
-        super.update(time);
-    }
-}
+//         case 'down':
+//             this.y = speed;
+//             break;
+
+//         case 'right':
+//             this.x = -speed;
+//             break;
+
+//         case 'left':
+//             this.x = speed;
+//             break;
+//     }
+// }
+// }
+
+// class Body {
+//     constructor({ imageName, speed }) {
+//         this.x = 0;
+//         this.y = 0;
+//         this.speed = speed;
+//         this.velocity = new Vector('down', 0); // vector of movement
+//         this.lastTime = 0; // time of last frame
+//         this.animations = {}; // container for animations
+
+//         const animationSheet = new CharacterSheet({ imageName: imageName });
+//         "walk_down, walk_up, walk_left, walk_right".split(',').forEach(name => {
+//             this.animations[name] = animationSheet.getAnimation(name);
+//     });
+//     this.stand('down')
+// }
+
+//     walk(direction) { // its making hero walk
+//         this.velocity.setDirection(direction, this.speed); // set speed
+//         this.view = this.animations["walk_" + direction]; // choose speed
+//         this.view.run() // run speed
+//     }
+
+//     stand(direction) { // its making hero stop
+//         this.velocity.setDirection(direction, 0);
+//         this.view = this.animations["walk_" + direction];
+//         this.view.stop();
+//     }
+
+//     update(time) {
+//         if(this.lastTime == 0) {
+//             this.lastTime = time;
+//               return;
+//         }
+
+//         this.x += (time - this.lastTime) * (this.velocity.x / 1000);
+//         this.y += (time - this.lastTime) * (this.velocity.x / 1000);
+//         this.lastTime = time;
+//         this.view.setXY(Math.trunc(this.x),Math.trunc(this.y))
+//         this.view.update(time);
+
+//     }
+// }
 
 
-// export default Hero;
+// class Player extends Body {
+
+//     constructor(control) {
+//         super({imageName: "player", speed: 50});
+//         this.control = control
+//     }
+//     update(time) {
+//         if(this.control.up) {
+//             this.walk('up');
+//         } else if (this.control.down) {
+//             this.walk('down'); 
+//         } else if (this.control.right) {
+//             this.walk('right'); 
+//         } else if (this.control.left) {
+//             this.walk('left'); 
+//         } else {
+//             this.stand(this.velocity.direction);
+//         }
+
+//         super.update(time);
+//     }
+// }
+
+
+// // export default Hero;
--- script.js
+++ script.js
@@ -1,20 +1,140 @@
 
-        var canvas = document.getElementById("gameboard");
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "green";
-        //Left side border
-        ctx.fillRect(0,0,40,550);
-        //right side border
-        ctx.fillRect(510,0,40,550);
-        //bottom border
-        ctx.fillRect(0,400,550,150);
 
-        // class Person{
-        //     constructor(health,direction){
-        //         this.health = health
-        //     }
-        // }
-        // class Hero extends Person{}
-        // class Mob extends Person{}
+var myGamePiece;
+var myObstacle;
 
-        
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myObstacle  = new component(10, 200, "green", 300, 120);    
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.querySelector("canvas"),
+    start : function() {
+        this.canvas.width = 550;
+        this.canvas.height = 550;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+//Left side border
+ctx.fillRect(0,0,40,550);
+//right side border
+ctx.fillRect(510,0,40,550);
+//bottom border
+ctx.fillRect(0,400,550,150);
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+//Left side border
+ctx.fillRect(0,0,40,550);
+//right side border
+ctx.fillRect(510,0,40,550);
+//bottom border
+ctx.fillRect(0,440,550,150);
+    }
+    // this.crashWith = function(otherobj) {
+    //     var myleft = this.x;
+    //     var myright = this.x + (this.width);
+    //     var mytop = this.y;
+    //     var mybottom = this.y + (this.height);
+    //     var otherleft = otherobj.x;
+    //     var otherright = otherobj.x + (otherobj.width);
+    //     var othertop = otherobj.y;
+    //     var otherbottom = otherobj.y + (otherobj.height);
+    //     var crash = true;
+    //     if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+    //         crash = false;
+    //     }
+    //     return crash;
+    // }
+}
+
+function updateGameArea() {
+    
+        myGameArea.clear();
+        myObstacle.update();
+        myGamePiece.x += myGamePiece.speedX;
+        myGamePiece.y += myGamePiece.speedY;    
+        myGamePiece.update();
+    }
+
+
+function moveup() {
+    if(myGamePiece.y>0)
+    myGamePiece.y-=5; 
+    else
+    myGamePiece.y+= 0;
+}
+
+function movedown() {
+    if(myGamePiece.y<400)
+    myGamePiece.y+=5; 
+    else{
+        myGamePiece.y+= 0;
+        console.log("can't move")
+    }
+}
+
+function moveleft() {
+    if(myGamePiece.x>40)
+    myGamePiece.x-=5; 
+    else
+    myGamePiece.x += 0;
+}
+
+function moveright() {
+    if(myGamePiece.x<510)
+    myGamePiece.x+=5; 
+    else
+    myGamePiece.x += 0;
+}
+
+// function clearmove() {
+//     myGamePiece.speedX = 0; 
+//     myGamePiece.speedY = 0; 
+// }
+
+function logKey(e) {
+    switch (e.key) {
+        case 'w':
+            moveup();
+            break;
+        case 's':
+            movedown()
+            break;
+        case 'a':
+            moveleft()
+            break;
+        case 'd':
+            moveright()
+            break;
+        default:
+            break;      
+    }
+}
+
+document.addEventListener('keydown', logKey);
