8cfbd5be320c79f1c44e0eaa2e8d890d9776d204
Vitalii Agapov <vitaly.agapov@outlook.com>
2019-11-05 09:40:44 -0500

    added levels

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let canvas = document.getElementById('game-board')
 
 canvas.width = 1000;
 canvas.height = 700;
-backgroundMusic = new sound ("./sounds/backgroundMusic.ogg");
+backgroundMusic = new sound("./sounds/backgroundMusic.ogg");
 backgroundMusic.play();
-coinSound = new sound ("./sounds/coinSound.wav");
-laserShoot = new sound ("./sounds/laserShoot.wav");
-boom = new sound ("./sounds/boom.wav");
+coinSound = new sound("./sounds/coinSound.wav");
+laserShoot = new sound("./sounds/laserShoot.wav");
+boom = new sound("./sounds/boom.wav");
 
 
 let ctx = canvas.getContext('2d')
@@ -29,7 +29,7 @@ let health = 3;
 
 
 class Player {
-  constructor(x,y,width,height){
+  constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -38,41 +38,41 @@ class Player {
   loadPlayer = () => {
     let img = new Image();
     img.src = './images/warrior.png'
-    
+
     img.onload = () => {
-      this.img = img; 
+      this.img = img;
       this.drawPlayer()
     }
   }
   movePlayer = (direction, value) => {
-    this[direction] += value; 
+    this[direction] += value;
   }
   drawPlayer = () => {
-    ctx.drawImage(this.img, this.x,this.y,this.width,this.height)
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
   }
-} 
+}
 
 class Monster {
-  constructor(x,y, width, height){
-      this.x =x;
-      this.y =y;
-      this.width = width;
-      this.height = height;
-      this.monster = null;
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.monster = null;
   }
-  loadRock = () =>{
-    let obstacleImg = new Image(); 
+  loadRock = () => {
+    let obstacleImg = new Image();
     obstacleImg.src = './images/rock.png'
     obstacleImg.onload = () => {
-      this.monster = obstacleImg; 
+      this.monster = obstacleImg;
       ctx.drawImage(this.monster, this.x, this.y, this.width, this.height)
     }
   }
-  loadDiamond = () =>{
-    let obstacleImg = new Image(); 
+  loadDiamond = () => {
+    let obstacleImg = new Image();
     obstacleImg.src = './images/diamond1.png'
     obstacleImg.onload = () => {
-      this.monster = obstacleImg; 
+      this.monster = obstacleImg;
       ctx.drawImage(this.monster, this.x, this.y, this.width, this.height)
     }
   }
@@ -83,12 +83,26 @@ class Monster {
     //console.log(this)
 
     this.y++;
-    if(this.y >700){
-      total-=100;
-      score.innerHTML=total;
-      health-=1;
+    if (this.y > 700) {
+      total -= 100;
+      score.innerHTML = total;
+      health -= 1;
       console.log(health);
-      
+
+      ladies.shift();
+    }
+  }
+
+  moveRockLvl2 = () => {
+
+    this.y+=2;
+
+    if (this.y > 700) {
+      total -= 100;
+      score.innerHTML = total;
+      health -= 1;
+      console.log(health);
+
       ladies.shift();
     }
   }
@@ -98,10 +112,10 @@ class Monster {
     //console.log(this)
 
     this.y++;
-    if(this.y >700){
-      
-    
-      
+    if (this.y > 700) {
+
+
+
       diamonds.shift();
     }
   }
@@ -123,26 +137,43 @@ hero.loadPlayer()
 
 
 
-function addRock(){
-  ladies.push(new Monster(Math.random()*canvas.width-5, 0, 32, 32))
+function addRock() {
+  ladies.push(new Monster(Math.random() * canvas.width - 5, 0, 32, 32))
 }
-function addDiamond(){
-  diamonds.push(new Monster(Math.random()*canvas.width-5, 0, 32, 32))
+function addDiamond() {
+  diamonds.push(new Monster(Math.random() * canvas.width - 5, 0, 32, 32))
 }
 
 
 function drawLadies() {
-  ladies.forEach(rock=> {
+  ladies.forEach(rock => {
     rock.loadRock()
     rock.moveRock()
     rock.drawRock()
-   
+
 
   })
 }
 
+function drawLadiesLvl2() {
+  ladies.forEach(rock => {
+    rock.loadRock()
+    rock.moveRockLvl2()
+    rock.drawRock()
+
+  })
+}
+
+
+function checkLvl() {
+  if (total <= 2000) {
+    drawLadies()
+  } if (total > 2000) {
+    drawLadiesLvl2()
+  }
+}
 function drawGem() {
-  diamonds.forEach(gem=> {
+  diamonds.forEach(gem => {
     gem.loadDiamond()
     gem.moveDiamond()
     gem.drawDiamond()
@@ -156,11 +187,11 @@ let lasers = [];
 function drawLaser() {
 
   if (lasers.length)
-  
+
     for (var i = 0; i < lasers.length; i++) {
       ctx.fillStyle = '#f00';
-      ctx.fillRect(lasers[i][0],lasers[i][1],lasers[i][2],lasers[i][3])
-      
+      ctx.fillRect(lasers[i][0], lasers[i][1], lasers[i][2], lasers[i][3])
+
     }
 }
 
@@ -172,7 +203,7 @@ function moveLaser() {
       lasers.splice(i, 1);
     }
   }
-  
+
 }
 
 
@@ -184,31 +215,31 @@ function hitTest() {
       console.log(lasers[i][1])
       // if (lasers[i][1] <= ladies[j].y && lasers[i][0] <= ladies[j].x) {
 
-        var rect2 = ladies[j]
+      var rect2 = ladies[j]
 
-        if (lasers[i][0] < rect2.x + rect2.width &&
-          lasers[i][0] + lasers[i][2] > rect2.x &&
-          lasers[i][1] < rect2.y + rect2.height &&
-          lasers[i][1] + lasers[i][3] > rect2.y) {
+      if (lasers[i][0] < rect2.x + rect2.width &&
+        lasers[i][0] + lasers[i][2] > rect2.x &&
+        lasers[i][1] < rect2.y + rect2.height &&
+        lasers[i][1] + lasers[i][3] > rect2.y) {
         remove = true;
-         ladies.splice(j, 1);
-         boom.play();
-         total+=100;
-         score.innerHTML=total;
-                               //  ladies.push([(Math.random() * 500) + 50, -45, enemy_w, enemy_h, speed]);
-       }
-     }
-     if (remove == true) {
-       lasers.splice(i, 1);
-  
-       remove = false;
-     }
-   }
- }
+        ladies.splice(j, 1);
+        boom.play();
+        total += 100;
+        score.innerHTML = total;
+        //  ladies.push([(Math.random() * 500) + 50, -45, enemy_w, enemy_h, speed]);
+      }
+    }
+    if (remove == true) {
+      lasers.splice(i, 1);
+
+      remove = false;
+    }
+  }
+}
 
 
-  
- 
+
+
 
 
 function sound(src) {
@@ -218,10 +249,10 @@ function sound(src) {
   this.sound.setAttribute("controls", "none");
   this.sound.style.display = "none";
   document.body.appendChild(this.sound);
-  this.play = function(){
+  this.play = function () {
     this.sound.play();
   }
-  this.stop = function(){
+  this.stop = function () {
     this.sound.pause();
   }
 }
@@ -231,59 +262,59 @@ function sound(src) {
 
 
 function checkCollision(aframe) {
- 
 
-  for(i = 0;i<ladies.length;i++){
+
+  for (i = 0; i < ladies.length; i++) {
     var rect1 = hero
     var rect2 = ladies[i]
-  
+
     if (rect1.x < rect2.x + rect2.width &&
       rect1.x + rect1.width > rect2.x &&
       rect1.y < rect2.y + rect2.height &&
       rect1.y + rect1.height > rect2.y) {
-        // collision detected!
-        
-        ladies.splice(i,1);
-        total+=100;
-        score.innerHTML=total;
-        coinSound.play();
-        console.log('collision')
-        window.cancelAnimationFrame(aframe)
-        return true;
+      // collision detected!
+
+      ladies.splice(i, 1);
+      total += 100;
+      score.innerHTML = total;
+      coinSound.play();
+      console.log('collision')
+      window.cancelAnimationFrame(aframe)
+      return true;
 
     }
-  
+
     return false;
   }
-  
-  }
 
-function checkCollision2(aframe){
+}
 
-  for(i = 0;i<diamonds.length;i++){
+function checkCollision2(aframe) {
+
+  for (i = 0; i < diamonds.length; i++) {
     var rect1 = hero
     var rect2 = diamonds[i]
-  
+
     if (rect1.x < rect2.x + rect2.width &&
       rect1.x + rect1.width > rect2.x &&
       rect1.y < rect2.y + rect2.height &&
       rect1.y + rect1.height > rect2.y) {
-        // collision detected!
-        
-        diamonds.splice(i,1);
-        total+=300;
-        score.innerHTML=total;
-        coinSound.play();
-        console.log('collision')
-        window.cancelAnimationFrame(aframe)
-        return true;
+      // collision detected!
+
+      diamonds.splice(i, 1);
+      total += 300;
+      score.innerHTML = total;
+      coinSound.play();
+      console.log('collision')
+      window.cancelAnimationFrame(aframe)
+      return true;
 
     }
-  
+
     return false;
   }
 
-}  
+}
 
 
 
@@ -294,23 +325,23 @@ function gameControls(e) {
   // if(e.key == 's'&&hero.y<510){
   //   hero.movePlayer('y', 15)
   // }
-  if(e.key == 'D'&&hero.x<934){
+  if (e.key == 'D' && hero.x < 934) {
     hero.movePlayer('x', 60)
   }
-  if(e.key == 'A'&&hero.x>5){
-    hero.movePlayer('x' ,-60)
+  if (e.key == 'A' && hero.x > 5) {
+    hero.movePlayer('x', -60)
   }
 
-  if(e.key == 'd'&&hero.x<934){
+  if (e.key == 'd' && hero.x < 934) {
     hero.movePlayer('x', 15)
   }
-  if(e.key == 'a'&&hero.x>5){
-    hero.movePlayer('x' ,-15)
+  if (e.key == 'a' && hero.x > 5) {
+    hero.movePlayer('x', -15)
   }
   if (e.keyCode == 32 && lasers.length <= laserTotal) {
     lasers.push([hero.x + 25, hero.y - 20, 4, 20]);
     laserShoot.play();
-  }  
+  }
 
 
 }
@@ -319,21 +350,21 @@ function gameControls(e) {
 
 
 
-let frames = 0; 
+let frames = 0;
 
 function animate() { //lifeblood of your canvas app.  This cycle forever, clears everything and redraws everything
 
   frames++;
 
   let aframe = window.requestAnimationFrame(animate)
-  ctx.clearRect(0,0,window.innerWidth, window.innerHeight)
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
 
 
   //road.drawRoad()
 
   hero.drawPlayer()
 
-  drawLadies()
+  checkLvl()
   drawGem();
 
   checkCollision();
@@ -344,37 +375,37 @@ function animate() { //lifeblood of your canvas app.  This cycle forever, clears
 
 
 
-  if(frames % 99 === 0){
-    
+  if (frames % 99 === 0) {
+
     addRock()
-    
+
   }
-  if(frames % 270 === 0){
-    
+  if (frames % 270 === 0) {
+
     addDiamond()
   }
 
-  
+
 
   // if(checkCollision(aframe)){ //I hit the Monster 
   //   window.cancelAnimationFrame(aframe)
   // }
 
-  if(health <= 0){
+  if (health <= 0) {
     //window.cancelAnimationFrame(aframe);
     //confirm("you lose")
     //if(confirm("you lose")) document.location = 'http://stackoverflow.com/';
   }
 
 
-  }
+}
 
 
-  
-  
-  //console.log(ladies.length)
 
-function Start(){
+
+//console.log(ladies.length)
+
+function Start() {
 
   setTimeout(animate, 1000)
 }
